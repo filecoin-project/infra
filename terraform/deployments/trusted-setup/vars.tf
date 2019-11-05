4d667df05c34af9e1119c35f64635854e3f64c35
Travis Person <travisperson@users.noreply.github.com>
2019-11-05 14:27:53 -0800

    Update rsyncbox

--- terraform/deployments/trusted-setup/vars.tf
+++ terraform/deployments/trusted-setup/vars.tf
@@ -3,6 +3,19 @@ variable "project_id" {
   default     = ""
 }
 
+variable "compute_public_key" {
+  description = "public key for the compute instance to talk to the rsyncbox"
+}
+
+# TODO migrate this subnet to terraform
+variable "subnet_id" {
+  description = "subnet_id the compute instance is on"
+}
+
+variable "zone_id" {
+  description = "zone_id to create the rsync record under"
+}
+
 variable "packet_auth_token" {
   description = "packet api token"
 }
