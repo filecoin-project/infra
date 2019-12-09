3ad119a8f1fd0b7831d4f224d510793628101a20
ognots <ognots@protonmail.com>
2019-12-09 15:47:36 -0500

    x1 device for trusted-setup

--- terraform/deployments/trusted-setup/main.tf
+++ terraform/deployments/trusted-setup/main.tf
@@ -1,5 +1,5 @@
 resource "packet_device" "ams1_m2" {
-  count               = 1
+  count               = 0
   hostname            = "trusted-setup-m2-${count.index}"
   plan                = "m2.xlarge.x86"
   facilities          = ["ams1"]
@@ -78,4 +78,21 @@ resource "aws_volume_attachment" "trusted_setup_runner" {
   volume_id    = "${aws_ebs_volume.trusted_setup_runner.id}"
   instance_id  = "${aws_instance.trusted-setup-runner.id}"
   force_detach = false
+}
+
+resource "aws_instance" "trusted_setup_runner_x1" {
+  count         = 1
+  ami           = "ami-05dd872834847c880"
+  instance_type = "x1e.16xlarge"
+  key_name      = "${var.aws_key_pair}"
+  subnet_id     = "${data.aws_subnet.subnet.id}"
+
+  vpc_security_group_ids = [
+    "${aws_security_group.trusted-setup-runner.id}",
+  ]
+
+  tags {
+    Terraform = "yes"
+    Name      = "trusted-setup-runner-x1-${count.index}"
+  }
 }
